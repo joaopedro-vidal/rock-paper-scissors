e1df0790f1b7a1caea281517950f294f0c7c781c
joaopedro-vidal <joaopedro.23nl@gmail.com>
2023-10-07 01:43:49 +0200

    feat: adding and removing class by js

---
+++ moveonscreen.js
@@ -0,0 +1,10 @@
+var button = document.getElementById("startbutton"); 
+
+button.addEventListener("click", function(){
+    var container = document.getElementById("startscreen");
+    container.classList.toggle("hide");
+
+    var containerGame = document.getElementById("gamescreen")
+    containerGame.classList.toggle("hide__off")
+
+});
