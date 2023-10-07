8fff01b4f571e0269fe3d194b032c2beda57f863
GuitounYoucef <68856205+GuitounYoucef@users.noreply.github.com>
2023-10-07 20:04:50 +0100

    container Result update

--- gameon.js
+++ gameon.js
@@ -1,5 +1,3 @@
-
-const buttonsSelector = document.querySelectorAll(".game__button");
 const containerResult = document.querySelector(".container__results")
 let containerHidden = false;
 const options = ['Rock', 'Paper', 'Scissors']
@@ -7,7 +5,6 @@ const options = ['Rock', 'Paper', 'Scissors']
 let playerScore = 0;
 let compterScore = 0;
 let round = 0;
-
 
 function updateScoreDisplay() {
 
@@ -31,43 +28,25 @@ function updateScoreDisplay() {
 
 document.addEventListener("DOMContentLoaded", updateScoreDisplay())
 
-buttonsSelector.forEach((button, index) => {
-    button.addEventListener("click", () => {
-        const buttonIndex = index;
-        play(buttonIndex);
-        if (!containerHidden) {
-            containerResult.classList.add("hide__off");
-            containerHidden = true;
-        }
-    });
-});
-
-
 function play(playerChoice) {
     const computerChoice = ramdomNumber(0, 2);
     document.getElementById('computer-choice').innerHTML = '('+options[computerChoice]+')';
     document.getElementById('player-choice').innerHTML = '('+options[playerChoice]+')';
-
     if ((computerChoice == 0 && playerChoice == 2) ||
         (computerChoice == 1 && playerChoice == 0) ||
         (computerChoice == 2 && playerChoice == 1)) {
         compterScore++;
         result("You lost! I'll be a prisioner forever", + computerChoice);
-   
-
-
     } else if (computerChoice == playerChoice) {
-
         result("Your weapon has the same power, try again! ", computerChoice);
-
-
     } else {
         playerScore++;
         result("You defeated Toad, now I'm free! ", computerChoice);
-
-
     }
-
+    if (!containerHidden) {
+        containerResult.classList.add("hide__off");
+        containerHidden = true;
+    }
 }
 
 function result(message, computerChoice) {
